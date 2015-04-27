c1c9b9d33dc8ec52f8ac604394edd1e298420b3a
Daniel Esqueda <dan.a.esqueda@gmail.com>
2015-04-27 14:44:48 -0700

    finished assignment 1

---
+++ day1/A1/calc.js
@@ -0,0 +1,19 @@
+// function to add two numbers
+exports.addNums = function(num1, num2) {
+  return num1 + num2;
+}
+
+// function to subtract two numbers
+exports.subNums = function(num1, num2) {
+  return num1 - num2;
+}
+
+// function to multiply two numbers
+exports.multNums = function(num1, num2) {
+  return num1 * num2;
+}
+
+// function to divide two numbers
+exports.divNums = function(num1, num2) {
+  return num1 / num2;
+}
