912c80997e3ca0fd8943d869e68cd54f20823d7b
Daniel Esqueda <dan.a.esqueda@gmail.com>
2015-05-07 15:22:55 -0700

    setup for simple persistence server

---
+++ day9/a2wk2/gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jscs');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+
+  grunt.initConfig({
+    jshint: {
+
+      all: {
+        options: {
+          node: true,
+          mocha: true,
+          expr: true,
+        },
+        src: ['./*.js', './test/**/*.js']
+      },
+    },
+
+    jscs: {
+      all: {
+        src: ['./*.js', './test/**/*.js']
+      }
+    },
+
+    simplemocha: {
+      all: {
+        src: ['./test/**/*_test.js']
+      }
+    }
+  });
+
+  grunt.registerTask('test', ['simplemocha']);
+  grunt.registerTask('style', ['jscs']);
+  grunt.registerTask('default', ['jshint', 'simplemocha']);
+
+};
